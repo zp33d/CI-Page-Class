b1cabb4bdffb2989ca2790c55e422ace12b3a25c
zp33d <a1@devmode.cf>
2018-04-18 23:22:05 +0800

    Create Page.php

---
+++ library/Page.php
@@ -0,0 +1,19 @@
+<?php defined('BASEPATH'); OR exit('No direct script access allowed');
+
+class Page {
+
+    protected $CI;
+
+    public function __construct()
+    {
+        // Set CI Supervariable
+        $this->CI =& get_instance();
+
+    }
+
+    public function build()
+    {
+
+    }
+
+}
